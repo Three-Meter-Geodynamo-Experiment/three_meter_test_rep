8c7668dc99efd4e608e24c326a4a1587dd8e9127
Sarah Burnett <sarahburnett@eduroam-108034.grenet.fr>
2019-09-18 19:27:44 +0200

    fixed it ???????????????????

--- a_times_b.m
+++ a_times_b.m
@@ -1,10 +1,6 @@
 function [result] = a_times_b(a,b)
 %A_TIMES_B returns a pruduct of two inputs
 % made by Artur Perevalov 09/2019
-c = 0; 
-for idx = 1:a
-    c = c+b;
-end
-%i fixed it??????????????
+results = a*b;
     
 end
