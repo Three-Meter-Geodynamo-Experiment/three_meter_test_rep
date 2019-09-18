3defd59e0ebee79fb8b3eb9eb0f06fde791e1ee7
Heidi Myers <heidimyers@wireless-10-104-5-130.umd.edu>
2019-09-18 13:18:41 -0400

    Fixing the bullshit

--- a_square.m
+++ a_square.m
@@ -1,7 +1,7 @@
 function [result] = a_square(a)
 %A_SQUARE returns square of the objects in input
 % made by Artur Perevalov 09/2019
-result = a.^3;
+result = a.^2; %fixed by Heidi Myers 09/18/2019
 % comment one
 end
 
