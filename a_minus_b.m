797536242191ab01d90196bc7149eb5f38d7a936
Artur Perevalov <arturperevalov@Arturs-MacBook-Pro-15.local>
2019-09-18 15:11:49 -0400

    fixing the factor of 2 in minus code

--- a_minus_b.m
+++ a_minus_b.m
@@ -2,7 +2,7 @@ function [result] = a_minus_b(a,b)
 %A_MINUS_B returns a minus b
 % made by Artur Perevalov 09/2019
 %fixed by Ruben Rojas changed + to - 09/2019
-result = a - 2*b;
+result = a - b;
 % comments three
 end
 
