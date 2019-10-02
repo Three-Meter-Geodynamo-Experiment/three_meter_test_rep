81be2beccfcc03b778e1aae566c6aa518c406f68
Sarah Burnett <sarahburnett@eduroam-109227.grenet.fr>
2019-10-02 09:52:58 +0200

    distance.m

--- distance.m
+++ distance.m
@@ -1,5 +1,5 @@
 function len = distance(a,b)
-% an unknown author wrote this code
+% Sarah of red hair and large nose created this in the year 2019 in the month of September
 len = sqrt(a_square(a_minus_b(a,b)));
 
 end
